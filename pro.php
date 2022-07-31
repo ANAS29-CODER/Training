fd457d2d99b087d0ddc52bbc02fd8702b5a4e7f4
Anas Ouda <anasouda43@gmail.com>
2022-07-31 14:28:43 +0300

    malek

---
+++ pro.php
@@ -0,0 +1,136 @@
+<?php
+
+include('layouts/Header.php');
+
+
+
+?>
+
+
+
+
+<div class="add-pro-to-cart">
+    <div class="products">
+       
+            <div class="row">
+                <div class=" col-sm-6  first">
+                    <div class="pro-img">
+                        <img src="Images/products/kIKu3yxsrrOf7AZrKUl6hg9dDyt9WMIeytJ7cIEr.png" alt="">
+                    </div>
+                    <div class="semi-product">
+                        <img src="Images/products/small.png" alt="">
+                        <img src="Images/products/small.png" alt="">
+                        <img src="Images/products/small.png" alt="">
+                        <img src="Images/products/small.png" alt="">
+
+                    </div>
+                </div>
+
+
+                <div class=" col-sm-6  product-details">
+                    <p>مشروب شعير بفاريا سادة 500 مل <span>Bavaria Orig .. </span></p>
+
+
+                    
+
+                    <div class="saler all">
+                        <p>مراسلة البائع</p>
+                    </div>
+                    <hr>
+
+
+                  
+                        <div class="prices all">
+                            <div class="price">
+
+                                <p> السعر : <span class="before-sale">225 <img src="Images/products/₪.png" alt=""></span></p>
+
+                                <p> السعر بعد الخصم : <span class="after-sale">215 <img src="Images/products/₪red.png" alt=""></span></p>
+
+                            </div>
+                            </div>
+                            <hr>
+
+                            <div class="product-quantity">
+                                <div class="word">
+                                    <p>الكمية :</p>
+                                </div>
+
+
+                                <div class="input-group ">
+
+                                    <button class="form-control click   minus"> <span>-</span> </button>
+                                    <button class="form-control click   number"><span>1</span></button>
+                                    <button class="form-control click   plus"><span>+</span></button>
+
+
+                                </div>
+
+                              
+
+                            </div>
+                           
+
+                       
+                        <hr>
+
+
+                        <div class="total-product-price">
+                            <p>السعر الكلي :<span>215 <img src="Images/products/₪red.png" alt=""></span></p>
+                        </div>
+
+
+
+                        <div class="cartadd">
+                            <button class="form-control addcart" style="font-size: 18px;">
+                                <i class="fa fa-shopping-basket" style="margin-left: 10px;"></i>
+                                اضافة الى للسلة </button>
+                                <button class="form-control addcart" style="font-size: 18px;">
+                                <i class="fa fa-shopping-basket" style="margin-left: 10px;"></i>
+                                اشتري الأن </button>
+                        </div>
+
+                        <div class="links">
+                            <p>مشاركة :</p>
+                            <div class="icons">
+
+                                <a href="#"><img src="Images/you.png" alt=""></a>
+                                <a href="#"><img src="Images/products/Group 29.png" alt=""></a>
+                                <a href="#"><img src="Images/twitter.png" alt=""></a>
+                                <a href="#"><img src="Images/face.png" alt=""></a>
+                                <a href="#"><img src="Images/tik.png" alt=""></a>
+
+                            </div>
+                        </div>
+
+
+
+
+                   
+
+
+
+
+                </div>
+
+
+               
+
+            </div>
+
+
+          
+      
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+       
